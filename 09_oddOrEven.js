e63be4a8cc8ddd8f56f0d2e6dd918805f0739012
absmohamed <cabdullahidaud@Hotmail.com>
2019-09-16 18:38:18 +1000

    Completed 09_oddOrEven

--- 09_oddOrEven.js
+++ 09_oddOrEven.js
@@ -10,6 +10,24 @@ If the input array is empty consider it as: [0] (array with a zero).
 
 function oddOrEven(array) {
     //enter code here
+    let count = 0
+    if (array.length === 0) {
+        return "even"
+    }
+
+    for(let a = 0; a < array.length; a++) {
+        count += array[a]
+    }
+
+    if(count % 2 === 0) {
+        return "even"
+    } else {
+        return "odd"
+    }
 }
+
+console.log(oddOrEven([5, 10, 15 ,34]));
+console.log(oddOrEven([4, 5, 6]));
+console.log(oddOrEven([]));
 
 module.exports = {oddOrEven};
